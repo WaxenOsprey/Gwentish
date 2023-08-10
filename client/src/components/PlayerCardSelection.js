d8f662b40aeb92b095a1f58320fcd1f84c034be1
WaxenOsprey <psc000001@gmail.com>
2023-08-09 17:30:57 -0700

    added key inputs

--- client/src/components/PlayerCardSelection.js
+++ client/src/components/PlayerCardSelection.js
@@ -1,14 +1,64 @@
-  import React, { useState } from "react";
-  import axios from 'axios';
-  import './PlayerCardSelection.css';
-  import Card from "./Card";
-  import handlePassRound from './HandlePassRound';
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import styled from "styled-components";
+import Card from "./Card";
+import handlePassRound from './HandlePassRound';
 
 
   const PlayerCardSelection = ({ activePlayer, setActivePlayer }) => {
     const [activePlayerSelectedHand, setActivePlayerSelectedHand] = useState([]);
     const [activePlayerSelectedCard, setActivePlayerSelectedCard] = useState(null);
     const [hoveredCard, setHoveredCard] = useState(null);
+
+
+    useEffect(() => {
+      const handleKeyPress = (e) => {
+        switch (e.key) {
+          case "ArrowLeft":
+            e.preventDefault();
+            if (activePlayer.hand.length >= 1) {
+              const currentIndex = activePlayer.hand.findIndex(card => activePlayerSelectedCard && activePlayerSelectedCard.id === card.id);
+              const previousIndex = (currentIndex - 1 + activePlayer.hand.length) % activePlayer.hand.length;
+              setActivePlayerSelectedCard(activePlayer.hand[previousIndex]);
+            }
+            break;
+          
+          case "ArrowRight":
+            e.preventDefault();
+            if (activePlayer.hand.length >= 1) {
+              const currentIndex = activePlayer.hand.findIndex(card => activePlayerSelectedCard && activePlayerSelectedCard.id === card.id);
+              const nextIndex = (currentIndex + 1) % activePlayer.hand.length;
+              setActivePlayerSelectedCard(activePlayer.hand[nextIndex]);
+            }
+            break;
+          
+          case "Enter":
+            if (activePlayerSelectedCard) {
+              handleChosenCardSubmission(e);
+            } else if (activePlayer.hand.length === 0) {
+              handleHandSubmit(e);
+            }
+            break;
+          
+          case " ":
+            if (activePlayer.hand.length > 1) {
+              handlePassRound(e, activePlayer, setActivePlayer);
+            }
+            break;
+    
+          default:
+            break;
+        }
+      };
+    
+      document.addEventListener("keydown", handleKeyPress);
+      return () => {
+        document.removeEventListener("keydown", handleKeyPress);
+      };
+    }, [activePlayerSelectedCard, activePlayer.hand.length]);
+    
+    
+    
 
     // Choosing hand from deck
 
@@ -96,51 +146,6 @@
     };
       
 
-    // const handlePassRound = async (e) => {
-    //   e.preventDefault();
-    //   try {
-    //     const passResponse = await axios.get('http://localhost:8080/api/gamestate/passRound');
-    //     console.log(passResponse.data);
-    //     console.log(activePlayer.name + " Has passed the round");
-    
-    //     try {
-    //       const roundOverResponse = await axios.get('http://localhost:8080/api/gamestate/isRoundOver');
-    //       console.log(roundOverResponse.data);
-    //       if (roundOverResponse.data === true) {
-    //         console.log("The round is over");
-    //         alert("The Round is Over!")
-    
-    //         try {
-    //           const gameOverResponse = await axios.get('http://localhost:8080/api/gamestate/isGameOver');
-    //           console.log(gameOverResponse.data);
-    //           if (gameOverResponse.data === true) {
-    //             console.log("The game is over");
-    //             alert("The Game is Over!")
-    //             // Perform actions for game over
-    //           } else {
-    //             console.log("The game is not over");
-    //             const toggleResponse = await axios.get('http://localhost:8080/api/gamestate/togglePlayer');
-    //             console.log(toggleResponse.data);
-    //             setActivePlayer(toggleResponse.data);
-    //           }
-    //         } catch (error) {
-    //           console.error(error);
-    //         }
-    //       } else {
-    //         console.log("The round is not over");
-    //         const toggleResponse = await axios.get('http://localhost:8080/api/gamestate/togglePlayer');
-    //         console.log(toggleResponse.data);
-    //         setActivePlayer(toggleResponse.data);
-    //       }
-    //     } catch (error) {
-    //       console.error(error);
-    //     }
-    //   } catch (error) {
-    //     console.error(error);
-    //   }
-    // };
-    
-
     const handleCardMouseEnter = (card) => {
       setHoveredCard(card);
     };
@@ -150,70 +155,130 @@
     };
 
     return (
-      <div>
-      {activePlayer && (
-      <h3>Choosing cards for Player: {activePlayer.name}</h3>
-      )}
-      <div className="PlayerCardSelection">
+      <Wrapper>
         {activePlayer && (
-          <>
-            {activePlayer.hand.length >= 1 ? (
-              <>
-                {activePlayer.hand.map((card) => (
-                  <div
-                    key={card.id}
-                    className={`card ${activePlayerSelectedCard && activePlayerSelectedCard.id === card.id ? 'active' : ''}`}
-                    onClick={() => handleChosenCardClick(card, card.name, card.power)}
-                    onMouseEnter={() => handleCardMouseEnter(card)}
-                    onMouseLeave={handleCardMouseLeave}
-                  >
-                    {/* <p>
-                      Name of Card: {card.name} || Power: {card.power}
-                    </p> */}
-                    <Card card={card} />
-                    {hoveredCard && hoveredCard.id === card.id && (
-                      <p className="flavor-text">{card.flavor}</p>
-                    )}
-                  </div>
-                ))}
-                <form onSubmit={handleChosenCardSubmission}>
-                  <input type="submit" value="Play Chosen Card" />
-                </form>
-              </>
-            ) : (
-              <>
-                {activePlayer.deck.map((card) => (
-                  <div
-                    key={card.id}
-                    className={`card ${activePlayerSelectedHand.some((selectedCard) => selectedCard.id === card.id) ? 'selected' : ''}`}
-                    onClick={() => handleClick(card, card.name, card.power)}
-                    onMouseEnter={() => handleCardMouseEnter(card)}
-                    onMouseLeave={handleCardMouseLeave}
-                  >
-                    {/* <p>
-                      Name of Card: {card.name} || Power: {card.power}
-                    </p> */}
-                    <Card card={card} />
-                    {hoveredCard && hoveredCard.id === card.id && (
-                      <p className="flavor-text">{card.flavor}</p>
-                    )}
-                  </div>
-                ))}
-                <form onSubmit={handleHandSubmit}>
-                  <input type="submit" value="Submit" />
-                </form>
-              </>
-            )}
-            {activePlayer.hand.length > 1 && (
-              <form onSubmit={(e) => handlePassRound(e, activePlayer, setActivePlayer)}>
-                <input type="submit" value="Pass Round" />
-              </form>
-            )}
-          </>
+          <Heading>Choosing cards for Player: {activePlayer.name}</Heading>
         )}
-      </div>
-      </div>
+        <CardGrid>
+          {activePlayer && (
+            <FlexContainer>
+              {activePlayer.hand.length >= 1 ? (
+                <>
+                  {activePlayer.hand.map((card) => (
+                    <CardContainer
+                      key={card.id}
+                      className={`card ${activePlayerSelectedCard && activePlayerSelectedCard.id === card.id ? 'selected' : ''}`}
+                      onClick={() => handleChosenCardClick(card, card.name, card.power)}
+                      onMouseEnter={() => handleCardMouseEnter(card)}
+                      onMouseLeave={handleCardMouseLeave}
+                    >
+                      <Card card={card} />
+                      {hoveredCard && hoveredCard.id === card.id && (
+                        <FlavorText>{card.flavor}</FlavorText>
+                      )}
+                    </CardContainer>
+                  ))}
+                  <form onSubmit={handleChosenCardSubmission}>
+                    <Button type="submit" value="Submit" />
+                  </form>
+                </>
+              ) : (
+                <>
+                  {activePlayer.deck.map((card) => (
+                    <CardContainer
+                      key={card.id}
+                      className={`card ${activePlayerSelectedHand.some((selectedCard) => selectedCard.id === card.id) ? 'selected' : ''}`}
+                      onClick={() => handleClick(card, card.name, card.power)}
+                      onMouseEnter={() => handleCardMouseEnter(card)}
+                      onMouseLeave={handleCardMouseLeave}
+                    >
+                      <Card card={card} />
+                      {hoveredCard && hoveredCard.id === card.id && (
+                        <FlavorText>{card.flavor}</FlavorText>
+                      )}
+                    </CardContainer>
+                  ))}
+                  <form onSubmit={handleHandSubmit}>
+                    <Button type="submit" value="Submit" />
+                  </form>
+                </>
+              )}
+              {activePlayer.hand.length > 1 && (
+                <form onSubmit={(e) => handlePassRound(e, activePlayer, setActivePlayer)}>
+                  <Button type="submit" value="Pass Round" />
+                </form>
+              )}
+            </FlexContainer>
+          )}
+        </CardGrid>
+      </Wrapper>
     );
   };
-
+  
+  const Wrapper = styled.div`
+    margin-left: 11.97%;
+    margin-right: 11.97%;
+  `;
+  
+  const Heading = styled.h3`
+    margin-left: 11.97%;
+    margin-right: 11.97%;
+    font-family: "Arial", sans-serif;
+    font-size: 24px;
+    color: #333;
+  `;
+  
+  const CardGrid = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+  `;
+  
+  const FlexContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+  `;
+  
+  const CardContainer = styled.div`
+    position: relative;
+    padding: 10px;
+    cursor: pointer;
+    box-shadow: 0px 0px 11px rgba(0, 0, 0, 0.16);
+    border-radius: 10px;
+    color: #333;
+  
+    &.selected,
+    &.active {
+      background-color: #f1f1f1;
+      box-shadow: 0px 0px 11px rgba(0, 0, 0, 0.16);
+      border-radius: 10px;
+    }
+  
+    &:hover .flavor-text {
+      visibility: visible;
+      opacity: 1;
+    }
+  `;
+  
+  const FlavorText = styled.p`
+    position: absolute;
+    top: -100%;
+    left: 0;
+    width: 100%;
+    padding: 10px;
+    background-color: rgba(0, 0, 0, 0.8);
+    color: #fff;
+    font-size: 14px;
+    line-height: 1.4;
+    visibility: hidden;
+    opacity: 0;
+    transition: visibility 0s, opacity 0.2s;
+    pointer-events: none;
+    z-index: 100;
+  `;
+  
+  const Button = styled.input`
+    margin-top: 10px;
+  `;
+  
   export default PlayerCardSelection;
